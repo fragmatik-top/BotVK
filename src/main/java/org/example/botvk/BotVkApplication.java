59929190f6e4c6c7245bee1a6608ab943a54662e
Evgen <fragmatik.Oo@yandex.ru>
2024-10-29 13:12:54 +0300

    Initial commit

---
+++ src/main/java/org/example/botvk/BotVkApplication.java
@@ -0,0 +1,13 @@
+package org.example.botvk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotVkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotVkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/botvk/BotVkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.botvk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotVkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
